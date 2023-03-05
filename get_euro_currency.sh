67ebb010a7bb4043e0c4e655dc4d615b10c2c466
Iulian Dornianu <iuliandornianu97@outlook.com>
2023-03-05 21:28:55 +0200

    Get Euro value Script - V1.1

--- get_euro_currency.sh
+++ get_euro_currency.sh
@@ -1,24 +1,43 @@
 #! /bin/bash
 
+USERNAME=idornianu
+VALUE=$(cat /home/$USERNAME/Output_final.txt)
 curl https://bnr.ro/Home.aspx >> /home/idornianu/output.txt
-cat /home/idornianu/output.txt | grep -i EUR >> /home/idornianu/output2.txt
-cat /home/idornianu/output2.txt | grep -o '4.92' >> /home/idornianu/Output_final.txt
-cat /home/idornianu/output2.txt | grep -o '4.93' >> /home/idornianu/Output_final.txt
-cat /home/idornianu/output2.txt | grep -o '4.94' >> /home/idornianu/Output_final.txt
-cat /home/idornianu/output2.txt | grep -o '4.95' >> /home/idornianu/Output_final.txt
-cat /home/idornianu/output2.txt | grep -o '4.96' >> /home/idornianu/Output_final.txt
-cat /home/idornianu/output2.txt | grep -o '4.97' >> /home/idornianu/Output_final.txt
-cat /home/idornianu/output2.txt | grep -o '4.98' >> /home/idornianu/Output_final.txt
-cat /home/idornianu/output2.txt | grep -o '4.99' >> /home/idornianu/Output_final.txt
-cat /home/idornianu/output2.txt | grep -o '5.00' >> /home/idornianu/Output_final.txt
-cat /home/idornianu/output2.txt | grep -o '4.88' >> /home/idornianu/Output_final.txt
-cat /home/idornianu/output2.txt | grep -o '4.87' >> /home/idornianu/Output_final.txt
-cat /home/idornianu/output2.txt | grep -o '4.86' >> /home/idornianu/Output_final.txt
-cat /home/idornianu/output2.txt | grep -o '4.85' >> /home/idornianu/Output_final.txt
-cat /home/idornianu/output2.txt | grep -o '4.84' >> /home/idornianu/Output_final.txt
-cat /home/idornianu/output2.txt | grep -o '4.83' >> /home/idornianu/Output_final.txt
-cat /home/idornianu/output2.txt | grep -o '4.82' >> /home/idornianu/Output_final.txt
-cat /home/idornianu/output2.txt | grep -o '4.81' >> /home/idornianu/Output_final.txt
-cat /home/idornianu/output2.txt | grep -o '4.80' >> /home/idornianu/Output_final.txt
-truncate -s 0 /home/idornianu/output*
+cat /home/$USERNAME/output.txt | grep -i EUR >> /home/$USERNAME/output2.txt
+cat /home/$USERNAME/output2.txt | grep -o '4.92' >> /home/$USERNAME/Output_final.txt
+cat /home/$USERNAME/output2.txt | grep -o '4.93' >> /home/$USERNAME/Output_final.txt
+cat /home/$USERNAME/output2.txt | grep -o '4.94' >> /home/$USERNAME/Output_final.txt
+cat /home/$USERNAME/output2.txt | grep -o '4.95' >> /home/$USERNAME/Output_final.txt
+cat /home/$USERNAME/output2.txt | grep -o '4.96' >> /home/$USERNAME/Output_final.txt
+cat /home/$USERNAME/output2.txt | grep -o '4.97' >> /home/$USERNAME/Output_final.txt
+cat /home/$USERNAME/output2.txt | grep -o '4.98' >> /home/$USERNAME/Output_final.txt
+cat /home/$USERNAME/output2.txt | grep -o '4.99' >> /home/$USERNAME/Output_final.txt
+cat /home/$USERNAME/output2.txt | grep -o '5.00' >> /home/$USERNAME/Output_final.txt
+cat /home/$USERNAME/output2.txt | grep -o '4.88' >> /home/$USERNAME/Output_final.txt
+cat /home/$USERNAME/output2.txt | grep -o '4.87' >> /home/$USERNAME/Output_final.txt
+cat /home/$USERNAME/output2.txt | grep -o '4.86' >> /home/$USERNAME/Output_final.txt
+cat /home/$USERNAME/output2.txt | grep -o '4.85' >> /home/$USERNAME/Output_final.txt
+cat /home/$USERNAME/output2.txt | grep -o '4.84' >> /home/$USERNAME/Output_final.txt
+cat /home/$USERNAME/output2.txt | grep -o '4.83' >> /home/$USERNAME/Output_final.txt
+cat /home/$USERNAME/output2.txt | grep -o '4.82' >> /home/$USERNAME/Output_final.txt
+cat /home/$USERNAME/output2.txt | grep -o '4.81' >> /home/$USERNAME/Output_final.txt
+cat /home/$USERNAME/output2.txt | grep -o '4.80' >> /home/$USERNAME/Output_final.txt
+truncate -s 0 /home/$USERNAME/output*
+
+if [ $VALUE >= 4.92 ];
+ then
+    echo "Todays Euro value is $VALUE . You can SELL Euro today." | mail -s "Euro is at a high price" iuliandornianu97@outlook.com
+ else
+    echo "Euro price is not good" >> /home/$USERNAME/Send_Euro_Data.log
+fi
+
+if [ $VALUE <= '4.88' ];
+ then
+    echo "Today's Euro value is $VALUE . You can BUY Euro today." | mail -s "Euro is at a low price" iuliandornianu97@outlook.com
+ else
+    echo "Euro price is not good" >> /home/$USERNAME/Send_Euro_data.log
+fi
+
+truncate -s 0 /home/$USERNAME/Output_final.txt
+
 exit
