0735bc1dbc5b5bda8867255f0200879801f7e882
Iulian Dornianu <idornianu@localhost.localdomain>
2024-06-25 18:25:05 +0300

    Modify_currency_script

--- get_euro_currency.sh
+++ get_euro_currency.sh
@@ -1,28 +1,31 @@
 #! /bin/bash
 
-USERNAME=idornianu
+USERNAME=$(whoami)
 VALUE=$(cat /home/$USERNAME/Output_final.txt)
 TIMESTAMP=`date "+%Y-%m-%d %H:%M:%S"`
 
-curl https://bnr.ro/Home.aspx >> /home/$USERNAME/output.txt
-cat /home/$USERNAME/output.txt | grep -i EUR >> /home/$USERNAME/output2.txt
-cat /home/$USERNAME/output2.txt | egrep -o '4.80|4.81|4.82|4.83|4.84|4.85|4.86|4.87|4.88|4.92|4.93|4.94|4.95|4.96|4.97|4.98|4.99|5.00' >> /home/$USERNAME/Output_final.txt
 truncate -s 0 /home/$USERNAME/output*
+curl https://bnr.ro/RSS_200003_EUR.aspx >> /home/$USERNAME/output.txt
+cat /home/$USERNAME/output.txt | grep "$(date "+%d-%m-%Y")" >> /home/$USERNAME/output_final.txt
 
-if [ $VALUE >= '4.92' ];
- then
-    echo "Valoarea Euro este $VALUE . Puteti vinde Euro." | mail -s "Euro are valoare ridicata" iuliandornianu97@outlook.com 
- else
-    echo "Euro price is not good. $VALUE EUR - $TIMESTAMP" >> /home/$USERNAME/Send_Euro_Data.log
-fi
+#cat /home/$USERNAME/output.txt | grep -i EUR >> /home/$USERNAME/output2.txt
+#cat /home/$USERNAME/output2.txt | egrep -o '4.80|4.81|4.82|4.83|4.84|4.85|4.86|4.87|4.88|4.92|4.93|4.94|4.95|4.96|4.97|4.98|4.99|5.00' >> /home/$USERNAME/Output_final.txt
+#truncate -s 0 /home/$USERNAME/output*
+#
+#if [ $VALUE >= '4.92' ];
+# then
+#    echo "Valoarea Euro este $VALUE . Puteti vinde Euro." | mail -s "Euro are valoare ridicata" iuliandornianu97@outlook.com 
+# else
+#    echo "Euro price is not good. $VALUE EUR - $TIMESTAMP" >> /home/$USERNAME/Send_Euro_Data.log
+#fi
+#
+#if [ $VALUE <= '4.88' ];
+# then
+#    echo "Valoarea Euro este $VALUE . Puteti cumpara Euro." | mail -s "Euro are valoare scazuta" iuliandornianu97@outlook.com
+# else
+#    echo "Euro price is not good. $VALUE EUR - $TIMESTAMP" >> /home/$USERNAME/Send_Euro_data.log
+#fi
 
-if [ $VALUE <= '4.88' ];
- then
-    echo "Valoarea Euro este $VALUE . Puteti cumpara Euro." | mail -s "Euro are valoare scazuta" iuliandornianu97@outlook.com
- else
-    echo "Euro price is not good. $VALUE EUR - $TIMESTAMP" >> /home/$USERNAME/Send_Euro_data.log
-fi
-
-truncate -s 0 /home/$USERNAME/Output_final.txt
+#truncate -s 0 /home/$USERNAME/Output_final.txt
 
 exit
