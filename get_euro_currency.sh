c86314e3a0a6f1f00f2678572f66c556f4187138
Iulian Dornianu <idornianu@localhost.localdomain>
2024-06-28 15:31:52 +0300

    Get Euro value Script - V2.3

--- get_euro_currency.sh
+++ get_euro_currency.sh
@@ -1,42 +1,48 @@
 #! /bin/bash
 
-#user who runs the script
+#variables
 USERNAME=$(whoami)
-
-#exchange rate for EURO
 EXCHGRT=$(awk '{print $1, $2, $3, $4}' /home/$USERNAME/output_final.txt)
-
-#today's date
 DATE=$(date "+%d-%m-%Y")
+DAY_OF_WEEK=$(date +%u)
+ATTEMPT=1
+MAX_ATTEMPTS=3
 
-#determine what day of the week it is ( from 1 to 5 )
-DAY_OF_WEEK=$( date +%u)
+#functions
+download_data() {
+  while [ $ATTEMPT -le $MAX_ATTEMPTS ]; do
+    curl https://bnr.ro/RSS_200003_EUR.aspx >> /home/$USERNAME/output.txt
+    if [ -s /home/$USERNAME/output.txt ]; then
+      return 0  # Success
+    else
+      echo "Attempt $ATTEMPT: Download failed or empty. Retrying..."
+      ((attempt++))
+      sleep 10  # Wait for 10 seconds before retrying
+    fi
+  done
+  echo "Failed to download data after $MAX_ATTEMPTS attempts."
+  return 1  # Failure
+}
 
-#set the date to search for based on the DAY_OF_WEEK value ( from 1 to 5 )
+#nitiate the download process and email send
+truncate -s 0 /home/$USERNAME/output*
+
 if [ "$DAY_OF_WEEK" -eq 1 ]; then
   YESTERDAY=$(date -d "last Friday" "+%d-%m-%Y")
 else
   YESTERDAY=$(date -d "yesterday" "+%d-%m-%Y")
 fi
 
-#cleanup old file contents
-truncate -s 0 /home/$USERNAME/output*
+download_data
 
-#gather the raw data from BNR
-curl https://bnr.ro/RSS_200003_EUR.aspx >> /home/$USERNAME/output.txt
-
-sleep 15
-
-#check for yesterday's date for the EURO value
 cat /home/$USERNAME/output.txt | grep $YESTERDAY >> /home/$USERNAME/output_final.txt
 
-#if the final output file is empty, don't send an email
 if [ ! -s /home/$USERNAME/output_final.txt ]; then
   echo "No value. Exiting."
   exit 1
 fi
 
-#gather the exchange rate from yesterday and the date from today, remove the HTML tags, send email
+#gather the exchange rate from yesterday and the date from today, remove the HTML tags, send email 2
 echo "$EXCHGRT RON - $DATE" | sed -e 's/<title>//; s/<\/title>//' | s-nail -s "Valoare EURO $(date "+%d-%m-%Y")" iuliandornianu97@outlook.com
 
 exit
